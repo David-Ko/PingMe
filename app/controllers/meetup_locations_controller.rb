b6ad160678eb08ec6ae8708a5a3840acfa5114cb
David Ko <dkoflow@gmail.com>
2019-02-13 19:15:35 -0800

    Implemented functionality to display created meetups

--- app/controllers/meetup_locations_controller.rb
+++ app/controllers/meetup_locations_controller.rb
@@ -3,14 +3,10 @@ class MeetupLocationsController < ApplicationController
     before_action :authenticate_user!
     
     def index
-        # current_location = CurrentLocation.where(user_id: current_user.id)
-        # locations = CurrentLocation.where(city: 'Vancouver');
-        # below not real code
-        # @locations = locations.filter(where distance between current locatoin and location is withing search params)
+        @user = current_user
+        @meetup_locations = @user.meetup_locations
     end
-
-
-
+    
     def create
         @meetup_location = MeetupLocation.new meetup_params
         @meetup_location.users << current_user
@@ -25,13 +21,8 @@ class MeetupLocationsController < ApplicationController
         @meetup_location = MeetupLocation.find params[:id]
     end
 
-
     private
-    
     def meetup_params
         params.require(:meetup_location).permit(:place_name, :place_address, :place_lat, :place_lng, :time)
     end
-
-
-
 end
